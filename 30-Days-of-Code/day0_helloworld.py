b6df83c1fbda57e46c9a6bacadd0b5d58829d3b8
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-06-23 22:17:40 +0300

    Day0 of 30 days of code

---
+++ 30-Days-of-Code/day0_helloworld.py
@@ -0,0 +1,11 @@
+# Task 
+# To complete this challenge, you must save a line of input from stdin to a variable, 
+# print Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+inputString = raw_input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print 'Hello, World.'
+
+# Write the second line of output
+print inputString
