2f642caa08675d784ef813d9accdcb951331c5a2
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-06-23 22:18:27 +0300

    Day1 of 30 days of code

---
+++ 30-Days-of-Code/day1_datatypes.py
@@ -0,0 +1,29 @@
+"""Task 
+Complete the code in the editor below. 
+The variables i, s, and d are already declared and initialized for you. 
+You must declare 3 variables: one of type int, one of type double, and one of type String. 
+Then you must read 3 lines of input from stdin and initialize your  variables. 
+Finally, you must use the + operator to perform the following operations:
+
+Print the sum of i plus your int variable on a new line.
+Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+Concatenate s with the string you read as input and print the result on a new line.
+"""
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+i2 = int(raw_input().strip())
+d2 = float(raw_input().strip())
+s2 = str(raw_input().strip())
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print i + i2
+# Print the sum of the double variables on a new line.
+print d + d2
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s +s2
