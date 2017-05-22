be053e86641ea42c4dae3c724854fb5072be0889
Phillis Kiragu <philliskiragu@users.noreply.github.com>
2017-05-22 17:34:09 +0300

    Birthday Cake Candles - Hackerank

---
+++ warm-up/birthday_cake_candles.py
@@ -0,0 +1,38 @@
+// I did this
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+height = map(int,raw_input().strip().split(' '))
+
+i = 0
+total = 0
+
+for j in height:
+    if j > i:
+        largest = j
+        i += 1
+    else:
+        largest = i
+        i += 1
+
+for j in height:
+    if j == largest:
+        total += 1
+        
+print total
+
+//Then I remembered the built in functions and this worked
+#!/bin/python
+
+import sys
+
+
+n = int(raw_input().strip())
+height = map(int,raw_input().strip().split(' '))
+
+tallest = max(height)
+
+print height.count(tallest)
